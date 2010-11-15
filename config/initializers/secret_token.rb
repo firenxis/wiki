29e898904167533327cf7445cdbb2759ad8c4acd
iortega <ignacio@firenxis.com>
2010-11-14 23:54:24 -0300

    ADD: version inicial proyecto rails con jquery, rspec y cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiki::Application.config.secret_token = '42d118ac5f691b444dad3826f2d37c0d951ee11f6e2e175c9d053dd1be8c1127c07b70bb7c7560dadd0c00d87fa127d85bb23a92406d9f40e6836a2c8eb19d4d'
